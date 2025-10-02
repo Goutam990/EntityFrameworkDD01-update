c9a2791fe8069d99a7a536ca83a049d5594a173f
Goutam Tiwari <tiwarigoutam8@gmail.com>
2025-10-02 23:50:57 +0530

    Updating Database using Ef

--- EntityFrameworkDD01/Controllers/HomeController.cs
+++ EntityFrameworkDD01/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.ComponentModel.DataAnnotations;
 using EntityFrameworkDD01.Models;
 
 namespace EntityFrameworkDD01.Controllers
@@ -43,6 +44,36 @@ namespace EntityFrameworkDD01.Controllers
             {
                 ViewBag.InsertMessage = "<script>alert('Data Not Inserted!!')</script>";
             }
+            }
+                return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            var row = db.Students.Where(model => model.Id == id).FirstOrDefault();
+            return View(row);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {
+
+            db.Entry(s).State = EntityState.Modified;
+            int a = db.SaveChanges();
+            if(a > 0)
+            {
+                //ViewBag.UpdateMessage = "<script>alert('Data Updated!!')</script>";
+                //TempData["UpdateMessage"] = "<script>alert('Data Updated!!')</script>";
+                TempData["UpdateMessage"] = "Data Updated!!";
+
+                return RedirectToAction("Index");
+
+                //ModelState.Clear();
+            }
+            else
+            {
+                ViewBag.UpdateMessage = "<script>alert('Data Not Updated!!')</script>";
             }
                 return View();
         }
