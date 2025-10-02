26360fa1d24426dc4443624e9d032d0568f978d2
Goutam Tiwari <tiwarigoutam8@gmail.com>
2025-10-02 13:06:43 +0530

    Add project files.

---
+++ EntityFrameworkDD01/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using EntityFrameworkDD01.Models;
+
+namespace EntityFrameworkDD01.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        StudentContext db= new StudentContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.Students.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ EntityFrameworkDD01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityFrameworkDD01.MvcApplication" Language="C#" %>
