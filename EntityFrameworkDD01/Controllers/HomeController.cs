c466c6f93b00e8a541121ac4941e2b819262f200
Goutam Tiwari <tiwarigoutam8@gmail.com>
2025-10-02 14:44:00 +0530

    CRUD operation. Create ActionMethod created. with HttpPost annotation  to ensure added added to the database too...

--- EntityFrameworkDD01/Controllers/HomeController.cs
+++ EntityFrameworkDD01/Controllers/HomeController.cs
@@ -18,5 +18,33 @@ namespace EntityFrameworkDD01.Controllers
             var data = db.Students.ToList();
             return View(data);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if(ModelState.IsValid == true)
+            {
+
+            db.Students.Add(s);
+            int a =  db.SaveChanges();
+            if(a> 0)
+            {
+                ViewBag.InsertMessage = "<script>alert('Data Inserted!!')</script>";
+                ModelState.Clear();
+            }
+            else
+            {
+                ViewBag.InsertMessage = "<script>alert('Data Not Inserted!!')</script>";
+            }
+            }
+                return View();
+        }
+
     }
+
 }
